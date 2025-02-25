123aee51e354993a8efbb559d9e9baa18c5fa7fc
Brynjar berg Ødegaard <helkion@proton.me>
2025-02-25 19:41:19 +0100

    mvp

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -6,13 +6,10 @@ const {
 } = require("../controller/transactionController");
 const verifyJwt = require("../middleware/verifyToken");
 
-// Apply JWT verification middleware to all routes
 router.use(verifyJwt);
 
-// Get all transactions for the logged-in user with pagination, sorting, and filtering
 router.get("/", getTransactions);
 
-// Get a specific transaction by ID
 router.get("/:id", getTransactionById);
 
 module.exports = router;
